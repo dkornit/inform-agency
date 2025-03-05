f2085ce99f2d6e6c4d64edf74547a216d844aab4
Dmytro Kornitskyi <d.kornitskyi@ukr.net>
2025-03-05 19:53:38 +0200

    Create urls for ListVieW and html files

---
+++ app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from app.models import Redactor
+from app.views import index, NewspaperListView, RedactorListView, TopicListView
+
+app_name = "app"
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("newspapers", NewspaperListView.as_view(), name="newspaper-list"),
+    path("redactors", RedactorListView.as_view(), name="redactor-list"),
+    path("topics", TopicListView.as_view(), name="topic-list"),
+
+]
