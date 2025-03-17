0f420d32b8d6d81c43e1286f19c73f1df7ce7554
Dmytro Kornitskyi <d.kornitskyi@ukr.net>
2025-03-17 12:51:40 +0200

    Prepare to deployment

--- inform_agency/wsgi.py
+++ inform_agency/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inform_agency.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inform_agency.settings.prod')
 
 application = get_wsgi_application()
